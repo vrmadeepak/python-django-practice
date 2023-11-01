4e1530dc6f1c1ff48432768aed65c4321cb3fc74
deepakpopclub <30309175+vrmadeepak@users.noreply.github.com>
2023-11-01 18:11:18 +0530

    view tasks api, update, view, delete task api

--- apps/todolist/urls.py
+++ apps/todolist/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from apps.todolist import views
 
 urlpatterns = [
-    # path("v1/tasks", views.AdditionAPIView.as_view(), name="add-api"),
-    # path("v1/tasks/<int:pk>/", views.AdditionAPIView.as_view(), name="add-api"),
+    path("v1/tasks", views.ViewAllTasksAPIView.as_view(), name="add-api"),
+    path("v1/tasks/<int:pk>/", views.TaskAPIView.as_view(), name="add-api"),
     path("v1/tasks/add", views.AddTaskAPIView.as_view(), name="add-api"),
 ]
--- apps/todolist/views.py
+++ apps/todolist/views.py
@@ -16,6 +16,131 @@ logger = logging.getLogger(__name__)
 __all__ = ("HealthCheck",)
 
 
+class ViewAllTasksAPIView(APIView):
+    permission_classes = [AllowAny]
+
+    def get(self, request, format=None):
+        try:
+            tasks = Task.objects.all().order_by('created_at')
+            data = [{
+                "title": task.title,
+                "description": task.description,
+                "status": "Incomplete" if not task.status else "Completed",
+                "date_created": task.created_at,
+                "date_updated": task.updated_at
+            } for task in tasks]
+            
+            return Response(
+                {
+                    "is_success": True,
+                    "message": None,
+                    "data": data,
+                },
+                status=status.HTTP_200_OK,
+            )
+        except Exception as ex:
+            return Response(
+                {"is_success": False, "message": ex.__str__(), "data": None},
+                status=status.HTTP_200_OK,
+            )
+
+
+class TaskAPIView(APIView):
+    permission_classes = [AllowAny]
+
+    def get(self, request, pk, format=None):
+        try:
+            task = Task.objects.get(id=pk)
+            data = {
+                "title": task.title,
+                "description": task.description,
+                "status": "Incomplete" if not task.status else "Completed",
+                "date_created": task.created_at,
+                "date_updated": task.updated_at
+            }
+            
+            return Response(
+                {
+                    "is_success": True,
+                    "message": None,
+                    "data": data,
+                },
+                status=status.HTTP_200_OK,
+            )
+        except Exception as ex:
+            return Response(
+                {"is_success": False, "message": ex.__str__(), "data": None},
+                status=status.HTTP_200_OK,
+            )
+        
+    def put(self, request, pk, format=None):
+        try:
+            title = request.data.get("title", "").strip().title()
+            description = request.data.get("description", "").strip()
+            task_status = request.data.get("status", None)
+
+            task = Task.objects.get(id=pk)
+
+            task_updated = False
+            if title and title != task.title:
+                task.title = title
+                task_updated = True
+            if description and description != task.description:
+                task.description = description
+                task_updated = True
+            if task_status and task_status != task.status:
+                task.status = task_status
+                task_updated = True
+            task.save()
+
+            data = {
+                "title": task.title,
+                "description": task.description,
+                "status": "Incomplete" if not task.status else "Completed",
+                "date_created": task.created_at,
+                "date_updated": task.updated_at
+            }
+            
+            return Response(
+                {
+                    "is_success": True,
+                    "message": "Updated" if task_updated else "Nothing to update",
+                    "data": data,
+                },
+                status=status.HTTP_200_OK,
+            )
+        except Exception as ex:
+            return Response(
+                {"is_success": False, "message": ex.__str__(), "data": None},
+                status=status.HTTP_200_OK,
+            )
+
+    def delete(self, request, pk, format=None):
+        try:
+            # title = request.data.get("title", "").strip().title()
+            # description = request.data.get("description", "").strip()
+            # task_status = request.data.get("status", None)
+
+            task = Task.objects.filter(id=pk).first()
+            if not task:
+                raise ValueError(f"Task with id: {pk} does not exist")
+            task.delete()
+            
+            return Response(
+                {
+                    "is_success": True,
+                    "message": "Deleted Successfully",
+                    "data": None,
+                },
+                status=status.HTTP_200_OK,
+            )
+        except Exception as ex:
+            return Response(
+                {"is_success": False, "message": ex.__str__(), "data": None},
+                status=status.HTTP_200_OK,
+            )
+
+
 class AddTaskAPIView(APIView):
     permission_classes = [AllowAny]
 
